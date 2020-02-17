6e13d2c5eb17cc59d60a2693429ef4e3b6730005
antaha88 <ahmed.taha@rsm.eg>
2020-02-17 17:08:38 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'Enter the clietID from Developer Portal',
-    'client_secret' => 'Enter the clientSecret from Developer Portal',
+    'client_id' => 'ABa2XHHbsmuu28fuEgp06cbekKUYJy4sWsgcobxf1x4eRqLdlT',
+    'client_secret' => 'LHBNDQZdbHKxQeTQTrnwjbPQHf1QrSMP4AeUujOZ',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
 )
